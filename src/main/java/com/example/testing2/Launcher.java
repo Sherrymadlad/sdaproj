140108057974d9630a7e48f26be95ce5d1f7d4d7
Sherrymadlad <shehryarhassan789@gmail.com>
2025-11-18 11:33:31 +0500

    Initial commit

---
+++ src/main/java/com/example/testing2/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.testing2;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
